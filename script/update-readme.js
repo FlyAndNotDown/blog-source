cc22d41e2576a107805c00e2a2010acd2a48edea
John Kindem <461425614@qq.com>
2019-05-23 13:58:12 +0800

    🎉 add some code

--- script/update-readme.js
+++ script/update-readme.js
@@ -19,18 +19,17 @@ const config = {
 
     // some regex
     regex: {
-        number: /^[1-9]\d*$/
+        number: /^[1-9]\d*.md$/
     }
 };
 
 // import modules
 const fs = require('fs');
 
-/**
- * get meta info by line
- * @param {string} line line string
- * @returns {string} meta info string
- */
+function readIntroduction() {
+    return fs.readFileSync(config.introductionPath).toString();
+}
+
 function getMetaInfoByLine(line) {
     const tokens = line.split(' ');
     let result = '';
@@ -38,25 +37,26 @@ function getMetaInfoByLine(line) {
     return result;
 };
 
-/**
- * main function
- */
-(function() {
-    // read introduction.md
-    let introduction = fs.readFileSync(config.introductionPath).toString();
-
+function getPostNames() {
     // read post dir
     let files = fs.readdirSync(config.postDirPath);
+    
     // if name is not match regex 'number', remove it
     let postNames = [];
     files.forEach((file) => { if (file.match(config.regex.number)) { return postNames.push(file); } });
+    
     // sort by number order
     postNames.sort((a, b) => {
-        const numberA = parseInt(a, 10);
-        const numberB = parseInt(b, 10);
+        const numberA = parseInt(a.split('.')[0], 10);
+        const numberB = parseInt(b.split('.')[0], 10);
         return numberA - numberB;
     });
 
+    // return result
+    return postNames;
+}
+
+function getMetaInfos(postNames) {
     // for every post name, ready its meta info
     let postMetaInfos = [];
     postNames.forEach((postName) => {
@@ -66,12 +66,37 @@ function getMetaInfoByLine(line) {
 
         // return meta info object
         return postMetaInfos.push({
-            key: getMetaInfoByLine(lines[1]),
-            title: getMetaInfoByLine(lines[2]),
-            date: getMetaInfoByLine(lines[3]),
-            labels: getMetaInfoByLine(lines[4]).split(' ')
+            key: getMetaInfoByLine(lines[1]).replace('\r', ''),
+            title: getMetaInfoByLine(lines[2]).replace('\r', ''),
+            date: getMetaInfoByLine(lines[3]).replace('\r', ''),
+            labels: getMetaInfoByLine(lines[4]).replace('\r', '').split(' ')
         });
     });
 
+    // return result
+    return postMetaInfos;
+}
+
+function drawTable(metaInfos) {
     // TODO
+}
+
+function mergeResult(introduction, tableSection) {
+    return introduction + tableSection;
+}
+
+function writeResultToReadmeFile(result) {
+    // TODO
+}
+
+/**
+ * main function
+ */
+(function() {
+    let introduction = readIntroduction();
+    let postNames = getPostNames();
+    let metaInfos = getMetaInfos(postNames);
+    let tableSection = drawTable(metaInfos);
+    let result = mergeResult(introduction, tableSection);
+    writeResultToReadmeFile();
 })();
